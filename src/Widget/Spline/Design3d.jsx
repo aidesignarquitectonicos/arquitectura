5faa2772cf1258b2d46055ceeb89d03679395f6b
Cordova Anthony <72144025+18-anth@users.noreply.github.com>
2024-05-14 18:45:22 -0500

    styles animate3d commit

--- src/Widget/Spline/Design3d.jsx
+++ src/Widget/Spline/Design3d.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Application } from '@splinetool/runtime';
+import { Box } from '@mui/material';
 
 function SplineModel() {
   const canvasRef = useRef(null);
@@ -11,7 +12,32 @@ function SplineModel() {
     return () => app.dispose(); // Cleanup the app when the component unmounts
   }, []);
 
-  return <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />;
+  return (
+    <>
+      <Box
+        sx={{
+          justifyContent: 'center',
+          width: '100%',
+          height: '50%',
+          padding: '10px'
+        }}
+      >
+        <canvas
+          className='appbarfotter'
+          ref={canvasRef}
+          style={{
+            width: '100%',
+            height: '100%',
+            borderBottomLeftRadius: '20px',
+            borderBottomRightRadius: '20px',
+            borderTopLeftRadius: '20px',
+            borderTopRightRadius: '20px',
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+        />
+      </Box>
+    </>
+  );
 }
 
 export default SplineModel;
